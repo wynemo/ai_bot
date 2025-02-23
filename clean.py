6a108dd155459d625b0087c91d3d0839cdd71e68
wynemo <ggdabin@gmail.com>
2025-02-23 23:10:23 +0800

    commit: 调整 BeautifulSoup 文本清理格式，使用换行分隔并保留原始格式

--- clean.py
+++ clean.py
@@ -8,12 +8,14 @@ def clean_html(html_text):
 
     # 使用BeautifulSoup清理
     soup = BeautifulSoup(text, 'html.parser')
-    text = soup.get_text()
+    text = soup.get_text(separator='\n', strip=True)
 
     # 移除多余空白
-    text = ' '.join(text.split())
+    # text = ' '.join(text.split())
 
     # 移除特殊字符和控制字符
-    text = re.sub(r'[\x00-\x1f\x7f-\x9f]', '', text)
+    # text = re.sub(r'[\x00-\x1f\x7f-\x9f]', '', text)
+    #
+    # print(text)
 
     return text
